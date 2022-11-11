732777d2f38ec761195193ffbf679ca2a6aedf5c
ericlinsd <ericlinswe@gmail.com>
2022-11-11 14:11:12 -0800

    reading tests

--- timeout-2-project/problems/07-postpone-with-args.js
+++ timeout-2-project/problems/07-postpone-with-args.js
@@ -23,7 +23,10 @@ slowPrintSum(2, 8); // prints '10' after 500 ms
 ***********************************************************************/
 
 function postponeWithArgs(cb, delay) {
-  // Your code here
+  const helper = (...args) => {
+      setTimeout(cb, delay);
+  };
+  return helper;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
