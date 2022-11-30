0fb1123673a3de216bf04fdc5a866818f7ead9f0
ericlinsd <ericlinswe@gmail.com>
2022-11-30 09:40:31 -0800

    teacher problem

--- inheritance/classes/teacher.js
+++ inheritance/classes/teacher.js
@@ -1,6 +1,22 @@
 const Person = require('./person');
 
-// Your code here
+class Teacher {
+  constructor (firstName, lastName, age, subject, yearsOfExperience) {
+      // invoking super function to access parent class properties
+      super(firstName, lastName, age);
+      this.subject = subject;
+      this.yearsOfExperience = yearsOfExperience;
+  };
+  static combinedYearsOfExperience (teachers) {
+      // invoking the reducer function to sum all years of experience
+      const totalYearsOfExp = teachers.reduce(function (acc, curr) {
+          const years = curr.yearsOfExperience;
+          return acc + years;
+      },0);
+      return totalYearsOfExp;
+  };
+
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
