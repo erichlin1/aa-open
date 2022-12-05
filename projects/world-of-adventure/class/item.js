c0fbadeaac5558787c0ffc7855d03a9625b3e650
ericlinsd <ericlinswe@gmail.com>
2022-12-05 12:48:03 -0800

    defined Item class

--- projects/world-of-adventure/class/item.js
+++ projects/world-of-adventure/class/item.js
@@ -1,8 +1,13 @@
 class Item {
-
-    // Fill this in
-
+    constructor (name, description) {
+        this.name = name;
+        this.description = description;
+    }
 }
+
+
+
+
 
 module.exports = {
   Item,
