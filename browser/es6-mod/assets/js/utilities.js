aa513ecb30f48cf4cb62d77f8b3640da8feba073
ericlinsd <ericlinswe@gmail.com>
2022-12-08 12:28:05 -0800

    comments

--- browser/es6-mod/assets/js/utilities.js
+++ browser/es6-mod/assets/js/utilities.js
@@ -1,5 +1,8 @@
 export const getIndex = () => {
     // intialize 'wrapper' variable with the element object (instance of a class) that has property value 'wrapper'
     const wrapper = document.getElementById('wrapper');
+    // accessing dataset property from wrapper which is an element of HTMLElement
+    // the dataset property represents the 'data-index' attribute, which is set to string 1.
+    // parseInt parses '1' to converts it to an integer
     return parseInt(wrapper.dataset.index);
 };
