126260f8f6bc12fb760b36a5df5f0e5ec058901e
ericlinsd <ericlinswe@gmail.com>
2022-12-08 18:34:42 -0800

    modified functions

--- browser/es6-mod-long/assets/js/manipulate.js
+++ browser/es6-mod-long/assets/js/manipulate.js
@@ -11,20 +11,21 @@ export function changeHeader() {
     const myName = 'Eric Lin';
     // Change the name in the h1 of the page to your name
     const headerElements = document.getElementById('header').children;
-    // finding index of H1 (although it's clear that <h1> tag is located at 0th index, I find it's best not assume)
-    const h1NodeIndex = headerElements.findIndex((node) => node.nodeName == 'H1');
-    // changing inner text of H1 nodeName
-
-    // headerElements which is an HTML collection is an 'array-like object'
-    // methods that iterate over arrays will not work for this object
-    headerElements.item(h1NodeIndex).innerText = `I am ${myName}`;
-
+    // changes inner text of H1 nodeName
+    for (let i = 0; i < headerElements.length; i += 1) {
+        let node = headerElements[i];
+        if (node.nodeName == 'H1') {
+            node.innerText = `I am ${myName}`;
+        };
+    };
+    // 'headerElements is a array-like object - either use reg. for loop, convert Array instance or an iterable 
     // * another approach would be to assign a unique id to the <h1> tag *
 }
 
 export function changeAboutMe() {
     /* Update the first paragraph in the About Me section with a small
      passage about yourself */
+    
     
     
 
