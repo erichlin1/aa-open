d294822a7a8a553c7f9cb69789bded7d093e92f6
ericlinsd <ericlinswe@gmail.com>
2022-12-09 14:08:04 -0800

    modified

---
+++ browser/es6-mod-long/assets/js/aboutMe.js
@@ -0,0 +1,5 @@
+export const aboutMeDesc = {
+    'work' : 'A digital marketer turned software engineer with a deep interest in products that excites the mind',
+    'hobby' : 'My favorite hobby is playing full court 5 on 5 basketball.',
+    'personal' : 'I love my girlfriend of 4 years and plan to propose to her soon. We just added a member to our family - oreo. A small fun sized shi-tzu that is a morning grinch.'
+}
--- browser/es6-mod-long/assets/js/manipulate.js
+++ browser/es6-mod-long/assets/js/manipulate.js
@@ -1,3 +1,7 @@
+import { aboutMeDesc } from './aboutMe.js';
+
+// why is aboutMeCollection not defined?
+
 export function changeTitle() {
     // Change the title of the page to "(Your name)'s Portfolio"
     const myName = 'Eric Lin';
@@ -10,24 +14,31 @@ export function changeTitle() {
 export function changeHeader() {
     const myName = 'Eric Lin';
     // Change the name in the h1 of the page to your name
-    const headerElements = document.getElementById('header').children;
-    // changes inner text of H1 nodeName
-    for (let i = 0; i < headerElements.length; i += 1) {
-        let node = headerElements[i];
+    const headerElements = Array.from(document.getElementById('header').children); // >> returns an HTML collection
+    headerElements.forEach((node) => {
         if (node.nodeName == 'H1') {
             node.innerText = `I am ${myName}`;
-        };
-    };
-    // 'headerElements is a array-like object - either use reg. for loop, convert Array instance or an iterable 
-    // * another approach would be to assign a unique id to the <h1> tag *
+        }
+    });
 }
+
+/* although 'aboutMeCollection' is obviously declared in global scope, it won't be referenced in
+function scope if the parameter has the same identifier. In this case, 'aboutMeCollection' is 
+declared, but not initialized resulting in a typeError from accessing a undefined property.
+*/
 
 export function changeAboutMe() {
-    /* Update the first paragraph in the About Me section with a small
-     passage about yourself */
-    
-    
-    
-
-    // Your code here
-}
+    let pCounts = 0;
+    let descCounts = aboutMeDesc.values().length;
+    /* add each property from aboutMeDesc to aboutMe section, in a separate paragraphs */
+    const aboutMeElement = document.getElementById('aboutMe');
+    const aboutMeArrCollection = Array.from(aboutMeElement.children);
+    // counts the number of 'P'
+    aboutMeArrCollection.forEach((child) => {
+        if (child.nodeName == 'H1') {
+            pCounts += 1;
+        }
+    });
+    // adds or subtracts paragraphs 
+    pCounts > descCounts ? /* delete p starting from the end */ : /* add p to the end */
+};
