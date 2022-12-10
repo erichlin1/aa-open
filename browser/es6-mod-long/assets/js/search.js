06cdbafc37ce58573db6aef22fa89159016e058e
ericlinsd <ericlinswe@gmail.com>
2022-12-10 02:13:55 -0800

    finished search function

--- browser/es6-mod-long/assets/js/search.js
+++ browser/es6-mod-long/assets/js/search.js
@@ -1,29 +1,25 @@
 export function findElementById(id) {
     // Return the element in the DOM with corresponding `id`
-
-    // Your code here
+    return document.getElementById(id);
 }
 
 export function findFirstElementOfTag(tag) {
     // Return the first occurence of an element of tag name `tag`
-
-    // Your code here
+    return document.getElementsByTagName(tag).item(0);
 }
 
 export function findFirstElementOfClass(cls) {
     // Return the first occurence of an element of class `cls`
-
-    // Your code here
+    return document.getElementsByClassName(cls).item(0);
 }
 
 export function findElementsOfTag(tag) {
     // Return an array of elements that have a tag name of `tag`
-
-    // Your code here
+    // returns non-live array of tag
+    return Array.from(document.getElementsByTagName(tag));
 }
 
 export function findElementsOfClass(cls) {
     // Return an array of elements that have are of class `cls`
-
-    // Your code here
+    return Array.from(document.getElementsByClassName(cls));
 }
