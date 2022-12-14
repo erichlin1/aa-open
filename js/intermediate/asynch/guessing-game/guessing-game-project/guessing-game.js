4074ec5f625b40993defa9b905b456c4b45a30a9
ericlinsd <ericlinswe@gmail.com>
2022-12-14 00:53:05 -0800

    guessing game project - completed

--- js/intermediate/asynch/guessing-game/guessing-game-project/guessing-game.js
+++ js/intermediate/asynch/guessing-game/guessing-game-project/guessing-game.js
@@ -1,4 +1,3 @@
-const { stdin, stdout } = require('process');
 const readline = require('readline');
 
 // creates a readline.Interface object, which question can be invoked
@@ -7,71 +6,74 @@ const rl = readline.createInterface({
     output: process.stdout
 })
 
-/*
+// generates a random number between the range of min and max specified by the user
 const randomInRange = (min, max) => {
-    return Math.random() * (max - min) + min;
+    return Math.floor(Math.random() * (max - min) + min);
 };
-*/
 
-const checkGuess = (min, max, ans) => {
-    console.log(min, max, ans);
-    if (ans > max) {
+// checks if the user input value is the same as random
+const checkGuess = (userGuess, random) => {
+
+    if (userGuess > random) {
         console.log('Too high');
         return false;
-    } else if (ans < min) {
+    } else if (userGuess < random) {
         console.log('Too low');
         return false;
-    } else {
+    } else if (userGuess == random) {
         console.log('Correct!');
         return true;
+    } else {
+        console.log('Please enter a valid entry - numbers only');
+        return false;
     }
+    
 };
 
-/* Local Test Cases
 
-console.log(checkGuess(5) == true);
-console.log(checkGuess(1) == false);
-console.log(checkGuess(7) == false);
-
-*/
-
-const askRange = () => {
-    askMin();
-}
-askRange();
 /* prompts user to enter min number */
 function askMin() {
     rl.question('Enter a min number: ', askMax);
 
 };
-/* prompts user to enter max number */
-/* passed in both min and max variables in the callback function because
-   min and max are not enclosed
-*/
+
+/* prompts user to enter max number. Min and Max are not enclosed. */
 function askMax(min) {
-    rl.question('Enter a max number: ', (max) => askGuess(min, max));
+    rl.question('Enter a max number: ', (max) => randomNumber(min, max));
 
 };
 
-function askGuess(min, max) {
+/* invokes random generator function and asks user to input the amount of attempts */
+function randomNumber(min, max) {
     console.log(`I'm thinking of a number between ${min} and ${max}...`);
-    rl.question('Enter a guess: ', (number) => {
-        const ans = Number(number);
-        const minNumber = Number(min);
-        const maxNumber = Number(max);
-        checkGuess(minNumber, maxNumber, ans) ? rl.close() : askGuess(min, max);
-    });
+    const minNumber = Number(min);
+    const maxNumber = Number(max);
+    const random = randomInRange(minNumber, maxNumber);
+    rl.question('specify the number of attempts: ', (attempts) => askGuess(random, attempts));
     
 };
 
+/* prompts user to guess a number */
+function askGuess(random, attempts) {
+    attempts -= 1;
+    rl.question('Enter a guess: ', (number) => {
+        const userGuess = Number(number);
+        let checked = checkGuess(userGuess, random);
+        if (attempts) {
+            console.log(`You have ${attempts} attempts left`);
+        } else {
+            console.log(`You have no more attempts - good try`);
+        }
+        if (checked || attempts == 0) {
+            rl.close();
+        } else {
+            askGuess(random, attempts);
+        };
+    });
+};
 
-
-
-/* Local Test Case 
-
-console.log(randomInRange(min, max) < max  && randomInRange(min, max) > min);
-
-*/
-
-
+const askRange = () => {
+    askMin();
+}
+askRange();
 
