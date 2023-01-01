599b57e7e93956f18b0623144043efd9831e26b5
Eric Lin <40250570+ericlinsd@users.noreply.github.com>
2022-12-31 17:02:36 -0800

    Update test.js
    bound `sayName` function to context `John`

--- tdd-oop/context/reg/bind/test.js
+++ tdd-oop/context/reg/bind/test.js
@@ -1,1 +1,8 @@
 // Your code here
+
+import Employee from './test.js';
+
+const john = Employee ('john wick', 'dog lover');
+const johnFullName = john.sayName.bind(john);
+
+setTimeout(johnFullName(), 2000);
